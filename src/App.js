a8fd298fd758fa0546f8a73d25e3ddea4a1affcd
Jasmine Davis <jasminedavis@new-host-4.home>
2020-11-24 16:52:29 -0500

    cleaner code maybe

--- src/App.js
+++ src/App.js
@@ -3,66 +3,43 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-
+// The class "square" creates a component of <Square/> that can be used in our JSX (think: fake HTML) below
 class Square extends React.Component {
   constructor(props){
     super(props);
     this.getFancy = this.getFancy.bind(this);
 
-    this.state={
-      color: "purple"
+    this.state={ // Initialize the color - when blank as at startup, it'll just use the CSS to set the color. This could be anything, purple, green, whatever.
+      color: ""
     }
   }
   getFancy() {
-    let fancyColor = '#'+ Math.floor(Math.random()*16777215).toString(16);
-    this.setState({color: fancyColor});
-    console.log("here");
+    let fancyColor = '#'+ Math.floor(Math.random()*16777215).toString(16); 
+    this.setState({color: fancyColor}); // Changes the state color to the random color from the line above.
+    // console.log("here");
    }
 
   render(){
     return (
       <div
-      onMouseOver={this.getFancy}
+      onMouseOver={this.getFancy} // runs the getFancy function on the specific moused-over element
       className="fancyDiv"
-      style={{backgroundColor: this.state.color}}
-      >{this.props.value}</div>
+      style={{backgroundColor: this.state.color}} // Changes the background color to the currently-set state of color
+      ></div> 
       )
     }
   }
 
 class App extends React.Component {
- constructor(props){
-  super(props);
-  
-  this.renderSquare = this.renderSquare.bind(this);
- }
- 
-renderSquare(props) {
-  return(
-<div
-     onMouseOver={this.getFancy}
-     className="fancyDiv"
-     style={{backgroundColor: this.state.color}}
-   >{props}</div>
-  );
-}
+
   render(){
  
-    // let divGrid = [];
-    // for(let i = 0; i < 100; i++){
-      // divGrid.push(this.square(i))
-      // AN IDEA: Set the color inside the for loop??
-      // let currentColor = "style={{backgroundColor: this.state.color}}"
-
-      // divGrid.push 
-      //   (<div
-      //     onMouseOver={this.getFancy}
-      //     className="fancyDiv"
-      //     style={{backgroundColor: this.state.color}}
-      //   ></div>)
-    // }
+    let divGrid = [];
+    for(let i = 0; i < 100; i++){
+    divGrid.push(<Square/>) // See above? Where we make Square? That's actually happening here!
+    }
     return (
-    <div className="bigDiv"><Square/><Square /></div>
+    <div className="bigDiv">{divGrid}</div>
   );
  }
 }
