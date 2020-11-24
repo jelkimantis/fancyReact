aeb2f4069a171a776451d145b4c3571bf643fea1
Jasmine Davis <jasminedavis@new-host-4.home>
2020-11-24 15:37:47 -0500

    added color changing for every box

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
- render(){
+ constructor(props){
+  super(props);
+  this.state={
+    color: "purple"
+  }
+  this.getFancy = this.getFancy.bind(this);
+ }
+ getFancy() {
+  let fancyColor = '#'+ Math.floor(Math.random()*16777215).toString(16);
+  this.setState({color: fancyColor});
+  console.log("here");
+ }
+
+  render(){
  
     let divGrid = [];
     
     for(let i = 0; i < 100; i++){
       divGrid.push 
         (<div
+          onMouseOver={this.getFancy}
           className="fancyDiv"
-        >X</div>)
+          style={{backgroundColor: this.state.color}}
+        ></div>)
     }
     return (
     <div className="bigDiv">{divGrid}</div>
