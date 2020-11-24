d21afaf8e0216b8bfc16570b0539d04adf8e6967
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 14:20:06 -0500

    updating to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+ render(){
+ 
+    let divGrid = [];
+    
+    for(let i = 0; i < 100; i++){
+      divGrid.push 
+        (<div
+          className="fancyDiv"
+        >X</div>)
+    }
+    return (
+    <div className="bigDiv">{divGrid}</div>
   );
+ }
 }
 
 export default App;
